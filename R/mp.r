99465ae106bfc54ad314f474eb06c43443f71ff8
ecastellon <eddy.castellon@mag.gob.ni>
2024-08-05 07:45:03 -0600

    xsql_s

--- R/mp.r
+++ R/mp.r
@@ -769,7 +769,7 @@ xsql_s <- function(x = character(), cam = "*") {
     stopifnot("arg. inadmisibles" = is_scalar_name(x) &&
                   filled_char(cam))
 
-    if ((!is_scalar(cam)) || (cam != "*")) {
+    if ((!is_scalar(cam)) || (cam[1] != "*")) {
         nm <- names(cam)
         if (filled(nm)) {
             cam <- paste(cam, nm, sep = " as ")
